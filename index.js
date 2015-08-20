647bc885a3b3c052bf8cd54f7dba5c44dbe0d5d1
Branwen Evans <=>
2015-08-20 14:17:51 +1200

    reading from db

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var cool = require('cool-ascii-faces');
 var mongodb = require('mongodb');
 var express = require('express');
 var app = express();
@@ -20,41 +19,93 @@ app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
 
-app.get('/insert', function (request, response) {
-    response.send(cool());
+app.get('/db', function (request, response) {
+
+
+    var findDocuments = function (db, callback) {
+        var collection = db.collection('blobs');
+        // Find some documents
+        collection.find({}).toArray(function (err, docs) {
+            assert.equal(err, null);
+            console.log("Found the following records");
+            console.dir(docs);
+            callback(docs);
+        });
+    }
+
     mongodb.MongoClient.connect(app.get('mongoUri'), function (err, db) {
         if (err) {
             console.log('Unable to connect to mongodb: ', err);
         }
         else {
             console.log('Connected to mongodb!');
-            var blobsToInsert = [{
-                name: 'testBlob',
-                head: 'Hello Blob',
-                body: 'This is some testing text!!',
-                created: Date.now()
-            }];
-            insertIntoCollection(db, 'blobs', blobsToInsert);
+
+            findDocuments(db, function () {
+                db.close;
+            });
+
+            //collection.find({}, function (err, docs) {
+            //    try {
+            //        if (err) {
+            //            console.log('Error querying %s: ', collectionName, err);
+            //        }
+            //        else {
+            //            console.log('Succesfully queried %s: ', collectionName);
+            //            console.log(docs);
+            //        }
+            //    }
+            //    catch (ex) {
+            //        console.log(ex);
+            //    }
+            //    finally {
+            //        db.close();
+            //    }
+            //});
+            console.log("%s contains: ", collectionName, results);
         }
     });
 });
 
-function insertIntoCollection(db, collectionName, documentsToInsert) {
-    var collection = db.collection(collectionName);
-    collection.insert(documentsToInsert, function (err, result) {
-        try {
-            if (err) {
-                console.log('Error inserting into %s: ', collectionName, err);
-            }
-            else {
-                console.log('Succesfully inserted into %s: ', collectionName, result);
-            }
+
+function insertDocuments(collectionName, documentsToInsert) {
+    mongodb.MongoClient.connect(app.get('mongoUri'), function (err, db) {
+        if (err) {
+            console.log('Unable to connect to mongodb: ', err);
         }
-        catch (ex) {
-            console.log(ex);
-        }
-        finally {
-            db.close();
+        else {
+            console.log('Connected to mongodb!');
+            var collection = db.collection(collectionName);
+            collection.insert(documentsToInsert, function (err, result) {
+                try {
+                    if (err) {
+                        console.log('Error inserting into %s: ', collectionName, err);
+                    }
+                    else {
+                        console.log('Succesfully inserted into %s: ', collectionName, result);
+                    }
+                }
+                catch (ex) {
+                    console.log(ex);
+                }
+                finally {
+                    db.close();
+                }
+            });
         }
     });
 }
+
+app.get('/insert', function (request, response) {
+    response.send('Inserting some data!');
+
+    var collectionName = 'blobs';
+
+    var documentsToInsert = [{
+        name: 'testBlob',
+        head: 'Hello Blob',
+        body: 'This is some testing text!!',
+        created: Date.now()
+    }];
+    insertDocuments(collectionName, documentsToInsert);
+});
+
